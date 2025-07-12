e530baa060d881002cdbefda750a813b37167fb3
adamdev <adamdevofficial@protonmail.com>
2025-07-12 18:07:09 -0400

    Update project version and add download link

--- src/Main.cs
+++ src/Main.cs
@@ -16,8 +16,8 @@ namespace NEP.Hitmarkers
         public const string Description = "Simple hitmarkers mod for BONELAB."; // Description for the Mod.  (Set as null if none)
         public const string Author = "Not Enough Photons"; // Author of the Mod.  (MUST BE SET)
         public const string Company = "Not Enough Photons"; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "2.8.3"; // Version of the Mod.  (MUST BE SET)
-        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+        public const string Version = "2.9.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = "https://thunderstore.io/c/bonelab/p/NotEnoughPhotons/Hitmarkers/"; // Download Link for the Mod.  (Set as null if none)
     }
     
     public class Main : MelonMod
