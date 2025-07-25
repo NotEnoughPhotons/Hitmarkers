7af4af00560f85469510bd7cbb72a0c4c1f02f72
adamdev <adamdevofficial@protonmail.com>
2025-07-25 17:29:47 -0400

    Update version to 2.10.0

--- src/Main.cs
+++ src/Main.cs
@@ -16,7 +16,7 @@ namespace NEP.Hitmarkers
         public const string Description = "Simple hitmarkers mod for BONELAB."; // Description for the Mod.  (Set as null if none)
         public const string Author = "Not Enough Photons"; // Author of the Mod.  (MUST BE SET)
         public const string Company = "Not Enough Photons"; // Company that made the Mod.  (Set as null if none)
-        public const string Version = "2.9.0"; // Version of the Mod.  (MUST BE SET)
+        public const string Version = "2.10.0"; // Version of the Mod.  (MUST BE SET)
         public const string DownloadLink = "https://thunderstore.io/c/bonelab/p/NotEnoughPhotons/Hitmarkers/"; // Download Link for the Mod.  (Set as null if none)
     }
     
